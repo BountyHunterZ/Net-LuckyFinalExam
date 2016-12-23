b645b7dbdc6a3d4aa7fa99ffa4545afb7efeeb87
zhongdian121 <ceo@myaiol.com>
2016-12-23 16:49:25 +0800

    Client wants some changes

--- drawMyLuck.aspx.cs
+++ drawMyLuck.aspx.cs
@@ -22,7 +22,7 @@ namespace LuckyFinalExam
         protected void Page_Load(object sender, EventArgs e)
         {
             Response.ClearContent();
-            Response.ContentType = "image/jpeg";
+            Response.ContentType = "image/PNG";
 
             string name = Request.Params["name"];
             string subject = Request.Params["subject"];
@@ -43,6 +43,7 @@ namespace LuckyFinalExam
             var totalSentence = expressions.Count;
             int luckyNum = finalNum % totalSentence;
             luckyNode = expressions.Item(luckyNum);
+            /*
             if (name == "钟典")
             {
                 luckyNode.ChildNodes.Item(0).InnerText = "多吉";
@@ -61,6 +62,7 @@ namespace LuckyFinalExam
                 luckyNode.ChildNodes.Item(2).InnerText = "何况是最漂亮的"+name+"呢";
                 luckyNode.ChildNodes.Item(3).InnerText = "——哲学家呆似萌";
             }
+            */
 
             var initImage = System.Drawing.Image.FromFile("C:/DrawMyLuck/BigLuck.png");
             resultGraphic = Graphics.FromImage(initImage);
@@ -70,7 +72,7 @@ namespace LuckyFinalExam
             drawDate(year, month, day);
       
             var ms = new MemoryStream();
-            initImage.Save(ms, ImageFormat.Jpeg);
+            initImage.Save(ms, ImageFormat.Png);
             Response.BinaryWrite(ms.ToArray());
             initImage.Dispose();
             Response.End();
@@ -78,17 +80,17 @@ namespace LuckyFinalExam
 
         private void drawLuckLevel()
         {
-            StringFormat stringFormat = new StringFormat();
-            stringFormat.Alignment = StringAlignment.Center;
-            stringFormat.LineAlignment = StringAlignment.Center;
 
-            Rectangle rect = new Rectangle(0, 200, 657, 240);
-
-            Font theFont = new Font("康煕字典體(Demo)", 43.97f, System.Drawing.FontStyle.Regular);
+            Font aFont = new Font("方正榜书行简体", 33.94f, System.Drawing.FontStyle.Regular);
+            Font bFont = new Font("方正榜书行简体", 61.93f, System.Drawing.FontStyle.Regular);
             Color theColor = Color.FromArgb(199, 68, 52);
             Brush theBrush = new SolidBrush(theColor);
-            resultGraphic.DrawString(luckyNode.ChildNodes.Item(0).InnerText, theFont, theBrush, rect, stringFormat);
- 
+            resultGraphic.DrawString("过", bFont, theBrush, 325, 175);
+            int startX = -10, gapX = 120;
+            for (var i = 0; i < 3; i++)
+            {
+                resultGraphic.DrawString(luckyNode.ChildNodes.Item(0).InnerText.Substring(i, 1), aFont, theBrush, startX + gapX * i, 245);
+            }
         }
 
         private void drawContext(string name, int type)
@@ -97,19 +99,37 @@ namespace LuckyFinalExam
             stringFormat.Alignment = StringAlignment.Center;
             stringFormat.LineAlignment = StringAlignment.Center;
 
-
-            Font theFont = new Font("微软雅黑", 6.48f, System.Drawing.FontStyle.Bold);
-            Color theColor = Color.FromArgb(0, 0, 0);
-            Brush theBrush = new SolidBrush(theColor);
+            float fontsize;
+            int startY, gapY;
 
             var nodes = luckyNode.ChildNodes;
+            switch (nodes.Count)
+            {
+                case 3: fontsize = 8.06f; startY = 440; gapY = 50; break;
+                case 4: fontsize = 6.13f; startY = 430; gapY = 42; break;
+                default: fontsize = 5.8f; startY = 420; gapY = 38; break;
+            }
             for(var i=1; i<nodes.Count; i++)
             {
-                Rectangle rect = new Rectangle(0, 420+42*(i-1), 657, 36);
+                Font aFont = new Font("微软雅黑", fontsize, System.Drawing.FontStyle.Bold);
+                Color aColor = Color.FromArgb(0, 0, 0);
+                Brush aBrush = new SolidBrush(aColor);
+
+                Rectangle rect = new Rectangle(0, startY+gapY*(i-1), 657, 36);
                 var sentence = nodes.Item(i).InnerText.Replace("（）", name);
-                resultGraphic.DrawString(sentence, theFont, theBrush, rect, stringFormat);
+                resultGraphic.DrawString(sentence, aFont, aBrush, rect, stringFormat);
             }
 
+            Font bFont = new Font("思源黑体 CN Heavy", 7.48f, System.Drawing.FontStyle.Bold);
+            Color bColor = Color.FromArgb(255, 255, 255);
+            Brush bBrush = new SolidBrush(bColor);
+
+            var bSize = resultGraphic.MeasureString(name + "的", bFont);
+            var waterMark = System.Drawing.Image.FromFile("C:/DrawMyLuck/watermark.png");
+            var totalWidth = (float)waterMark.Size.Width + bSize.Width - 4;
+            var startX = (int)((657.0 - totalWidth) / 2);
+            resultGraphic.DrawString(name + "的", bFont, bBrush, startX, 28);
+            resultGraphic.DrawImage(waterMark, startX + (int)bSize.Width -4, 20);
         }
 
         private void drawSubject(string subject)
@@ -122,7 +142,7 @@ namespace LuckyFinalExam
             Color theColor = Color.FromArgb(255,255,255);
             Brush theBrush = new SolidBrush(theColor);
 
-            Rectangle rect = new Rectangle(0, 40, 657, 100);
+            Rectangle rect = new Rectangle(0, 70, 657, 100);
 
             resultGraphic.DrawString("《"+subject+"》", theFont, theBrush, rect, stringFormat);
         }
@@ -133,13 +153,13 @@ namespace LuckyFinalExam
             stringFormat.Alignment = StringAlignment.Center;
             stringFormat.LineAlignment = StringAlignment.Center;
 
-            Font theFont = new Font("思源黑体 CN Regular", 4.98f, System.Drawing.FontStyle.Regular);
+            Font theFont = new Font("思源黑体 CN Regular", 4.23f, System.Drawing.FontStyle.Regular);
             Color theColor = Color.FromArgb(255, 255, 255);
             Brush theBrush = new SolidBrush(theColor);
 
-            Rectangle rect = new Rectangle(0, 116, 657, 32);
+            Rectangle rect = new Rectangle(0, 142, 657, 32);
 
-            resultGraphic.DrawString(year+"年"+month+"月"+day+"日", theFont, theBrush, rect, stringFormat);
+            resultGraphic.DrawString("考试时间 | "+year+"年"+month+"月"+day+"日", theFont, theBrush, rect, stringFormat);
         }
         
         private int encodeToNum(string input)
